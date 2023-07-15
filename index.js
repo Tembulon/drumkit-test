078a54cbb3880e4903485e38be1fa9dbb59be3c4
Tembs <MangaMadMate@gmail.com>
2023-07-15 15:32:03 +0300

    Update index.js
    Added keydown for sound

--- index.js
+++ index.js
@@ -1,61 +1,81 @@
-var numberOfButtons = document.querySelectorAll(".drum").length;
-for (var i = 0; i < numberOfButtons; i++ ){
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-document.querySelectorAll(".drum")[i].addEventListener("click", function handleClick() {
-   
-    var buttonInnerHTML = this.innerHTML ;
-    
-    switch (buttonInnerHTML) {
-        case "w":
-            var tom1  = new Audio('sounds/tom-1.mp3');
-            tom1.play();
-            break;
-        
-        case "a":
-            var tom2  = new Audio('sounds/tom-2.mp3');
-            tom2.play();
+for (var i = 0; i < numberOfDrumButtons; i++) {
 
-            break;
-        
-        case "s":
-            var tom3 =  new Audio('sounds/tom-3.mp3');
-            tom3.play();
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
-            break;
+    var buttonInnerHTML = this.innerHTML;
 
-        case "d":
-            var tom4  = new Audio('sounds/tom-4.mp3');
-            tom4.play();
+    makeSound(buttonInnerHTML);
 
-            break;
+    buttonAnimation(buttonInnerHTML);
 
-        case "j":
-            var snare  = new Audio('sounds/snare.mp3');
-            snare.play();
-
-            break;
-            
-        case "k":
-            var crash  = new Audio('sounds/crash.mp3');
-            crash.play();
-
-            break;
-
-        case "l":
-            var kick  = new Audio('sounds/kick-bass.mp3');
-            kick.play();
-
-            break;
-
-        default: console.log(innerHTML);
-            break;
-    }
-});
+  });
 
 }
 
-document.addEventListener("keydown",  function(){
-    alert("Key was pressed");
+document.addEventListener("keydown", function(event) {
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+
 });
 
- 
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+
+    default: console.log(key);
+
+  }
+}
+
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+}
